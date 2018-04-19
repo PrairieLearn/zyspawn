6e19c443c4e95f04fb59ea0e7f62098de0269822
cwlowder <clowder2@illinois.edu>
2018-04-19 13:56:14 -0500

    Updated message to be more clear

--- zygote.py
+++ zygote.py
@@ -205,7 +205,7 @@ def parseInput(command_input):
         if (getChildPid() != -1):
             # we already have a child
             message["success"] = False
-            message["message"] = "already contains worker"
+            message["message"] = "zygote already contains worker"
             return message
         setChildPid(os.fork())
         if (getChildPid() == 0):
